dedcef3346662b8bbb5164c420fc759dc1ef7618
Akbaraliii <91048215+Akbaraliii@users.noreply.github.com>
2023-07-13 22:04:25 +0900

    update

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch("https://chatgptclone-xw52.onrender.com/", {
+  const response = await fetch("https://chatgptclone-xw52.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
